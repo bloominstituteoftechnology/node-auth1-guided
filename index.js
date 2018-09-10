d00bb877ac1310c06b3879d172144383b67f763b
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-10 16:07:17 -0400

    Monday PR

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
+const db = require("./database/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+server.get("/", (req, res) => {
+  res.send("Its Alive!");
+});
+
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 3);
+
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(users => {
+      if (users && bcrypt.compareSync(creds.password, users.password)) {
+        res.status(200).send("Welcome");
+      } else {
+        res.status(401).json({ message: "You do not have access" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
 });
 
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
-  db('users')
-    .select('id', 'username')
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username", "password")
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+server.listen(3300, () => console.log("\nrunning on port 3300\n"));
