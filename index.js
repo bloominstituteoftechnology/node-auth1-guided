0ccaa14a66bfcafe209f276566b530c5a4d27437
farste <sfargali@gmail.com>
2018-09-17 17:06:45 -0400

    Stephen Fargali Followed along with lecture

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./database/dbConfig.js');
-
+const db = require("./database/dbConfig.js");
+var bcrypt = require("bcryptjs");
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+server.get("/", (req, res) => {
+  res.send("Its Alive!");
 });
 
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+server.post("/api/register", (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 3);
+    creds.password = hash;
+
+    db("users")
+      .insert(creds)
+      .then(ids => {
+        const id = ids[0];
+        res.status(201).json(id);
+      })
+      .catch(err => res.status(500).send(err));
+  }
+);
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+  db('users')
+  .where({username: creds.username})
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)){
+      res.status(200).send('Welcome')
+    } else {
+      res.status(401).json({message: 'No admittance'})
+    }
+  })
+})
+server.listen(3300, () => console.log("\nrunning on port 3300\n"));
