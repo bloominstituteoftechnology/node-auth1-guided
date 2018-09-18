77afa4e87a0abb7bb9451139695a5c05146eec3d
bri79319 <bri79319@gmail.com>
2018-09-18 02:25:58 -0400

    setting up admins

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sessionsConfig = {
   secret: "nobody tosses a dwarf!",
   cookie: {
     maxAge: 1 * 24 * 60 * 60 * 1000, // a day
-    secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    secure: false // only set cookies over https. Server will not send back a cookie over http.
   }, // 1 day in milliseconds
   httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
   resave: false,
@@ -61,7 +61,9 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).send("Welcome");
+        req.session.username = user.username;
+
+        res.status(200).send(`Welcome ${req.session.username}`);
       } else {
         res.status(401).json({ Error: "Cannot Authorize" });
       }
@@ -78,18 +80,38 @@ server.get("/setname", (req, res) => {
 });
 
 server.get("/greet", (req, res) => {
-  const name = req.session.name;
-  res.send(`hello ${req.session.name}`);
+  const name = req.session.username;
+  res.send(`hello ${name}`);
 });
 
 // protect this route, only authenticated users should see it
 server.get("/api/users", (req, res) => {
-  db("users") //password is optional//
-    .select("id", "username")
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+  // only send the list of users if the client is logged in
+  if (req.session && req.session.username) {
+    db("users") //password is optional//
+      .select("id", "username")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.status(401).json({ Error: "You shall not pass!!" });
+  }
+});
+
+server.get("/api/admins", (req, res) => {
+  // only send the list of users if the client is logged in
+  // user = { username 'foo', role: 'admin' }
+  if (req.session && req.session.role === "admin") {
+    db("users") //password is optional//
+      .select("id", "username")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.status(403).json({ Error: "You have no access to this resource" });
+  }
 });
 
 server.listen(3300, () => console.log("\nrunning on port 3300\n"));
