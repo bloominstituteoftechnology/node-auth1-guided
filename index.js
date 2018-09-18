580c216c919560aeb81dd0f136f2b778df2b1e99
bri79319 <bri79319@gmail.com>
2018-09-18 13:56:13 -0400

    code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
+// require your story library
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const db = require("./database/dbConfig.js");
 
@@ -16,13 +18,28 @@ const sessionsConfig = {
   }, // 1 day in milliseconds
   httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: "sessions",
+    sidfieldname: "sid",
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
 };
 
 server.use(session(sessionsConfig));
 
 server.use(express.json());
 server.use(cors());
+
+function protected(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ Error: "You shall not pass!!" });
+  }
+}
 
 server.get("/", (req, res) => {
   res.send("Its Alive!");
@@ -61,6 +78,8 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // grab roles from user
+        // req.session.roles = roles;
         req.session.username = user.username;
 
         res.status(200).send(`Welcome ${req.session.username}`);
@@ -85,21 +104,40 @@ server.get("/greet", (req, res) => {
 });
 
 // protect this route, only authenticated users should see it
-server.get("/api/users", (req, res) => {
+server.get("/api/users", protected, (req, res) => {
   // only send the list of users if the client is logged in
-  if (req.session && req.session.username) {
-    db("users") //password is optional//
-      .select("id", "username")
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => res.send(err));
-  } else {
-    res.status(401).json({ Error: "You shall not pass!!" });
-  }
+
+  db("users") //password is optional//
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
 });
 
-server.get("/api/admins", (req, res) => {
+server.get("/api/admins", protected, (req, res) => {
+  // grab the logged in user id from the session
+  if (req.session && req.session.userId) {
+    const userId = req.session.userId;
+    db("roles as r")
+      .join("user_roles as ur", "ur.role_id", "=", "r.id")
+      .select("r.role_name")
+      .where("ur.user_id", userId)
+      .then(roles => {
+        if (roles.includes("admin")) {
+          // have access
+        } else {
+          // bounced
+        }
+      });
+  }
+
+  // role can have many permissions
+  // a user can have many roles
+  // a user can have many permissions
+
+  // query the db and get the roles for the user
+
   // only send the list of users if the client is logged in
   // user = { username 'foo', role: 'admin' }
   if (req.session && req.session.role === "admin") {
@@ -111,6 +149,18 @@ server.get("/api/admins", (req, res) => {
       .catch(err => res.send(err));
   } else {
     res.status(403).json({ Error: "You have no access to this resource" });
+  }
+});
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('error logging out');
+      } else {
+        res.send('good bye');
+      }
+    });
   }
 });
 
