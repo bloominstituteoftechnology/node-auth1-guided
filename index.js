43b08295dd43dcd3027fc4af67b36eb7d480d115
vy3191 <venkyafhs@gmail.com>
2019-01-16 18:45:04 -0500

    Initial commit

--- index.js
+++ index.js
@@ -22,8 +22,34 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-server.post('api/register', (req,res) => {
-    
+server.post('/api/register', (req,res) => {
+    const user = req.body;
+    if(!user) res.status(400).json({Message: `Please enter a valid user name and password`});
+    console.log(user);
+    db('users').insert(user)
+    .then( ids => {
+       console.log('line31:',ids);
+       res.status(201).json({id:ids[0]});
+    }).catch(err => {
+       res.status(500).json({Message: `Failed to register at this time`});
+    })
+});
+
+server.post('/api/login', (req,res) => {
+    const userBody = req.body;
+    db('users')
+    .where('username', userBody.username)
+    .then( users => {
+       if(users.length && users[0].password === userBody.password) {
+          res.status(200).json({Message: `Correct`});
+       } else {
+          res.status(404).json({Message: `Invalid username and password`});
+       }
+    })
+    .catch(err => {
+         res.status(500).json({err: `Something went wrong`})
+    })
+     
 })
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
