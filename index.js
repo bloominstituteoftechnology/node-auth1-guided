bf74f7e04b1b1ee0fd71c054dbfa5985da79ede6
Jesse Theisen <theiseja@gmail.com>
2019-02-25 21:23:31 -0600

    Added login & register, works and tested in postman

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-module.js');
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-module.js");
 
 const server = express();
 
@@ -12,54 +12,56 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
 // Registration
-server.post('/api/register', (req, res) => {
-  //grab username and password from body
-  const creds = req.body
-  //generate the hash from user's passowrd
-  const hash = bcrypt.hashSync(creds.password, 14) //rounds is 2^x
-  //override the user.password with the hash
-  creds.password = hash
-  //save user to the database
-  db('users')
-    .insert(creds)
+server.post("/api/register", (req, res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db("users")
+    .insert(credentials)
     .then(ids => {
-      res.status(201).json(ids)
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
     })
-    .catch(err => json(err))
-})
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 // Login
-server.post('/api/login', (req, res) => {
-  //grab username and password from body
-  const creds = req.body
-  db('users')
+server.post("/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
     .where({ username: creds.username })
-    .first() //only get one record back
+    .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        //passwords match and user exists by that username
-        res.status(200).json({ message: 'Login Successful!' })
+        res.status(200).json({ Welcome: user.username });
       } else {
-        // Either the username is invalid or password is wrong
-        res.status(401).json({ message: 'Thou shalt not pass!' })
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
-    .catch(err => res.json(err))
-})
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
 
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
-  Users.find()
+  db('users')
+    .select('id', 'username')
     .then(users => {
-      res.json(users);
+      res.json(users)
     })
-    .catch(err => res.send(err));
-});
+    .catch(err => res.send(err))
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
