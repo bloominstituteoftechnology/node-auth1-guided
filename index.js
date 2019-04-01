7599e4ac36d37140391eac04bec0fe5ac4d38a79
Delba <delba.brown@gmail.com>
2019-04-01 07:47:13 +0100

    Hashing passwords with bcrypt

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
 
 const server = express();
 
@@ -11,12 +12,18 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   let user = req.body;
+
+  // generate has from user's passowrd
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n, where n is the 2nd arg in the function call
+
+  // override user.password with hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -27,16 +34,17 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
@@ -44,12 +52,44 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+// Protect this route, only authenticated users should see it
+// Middleware
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Crendentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Ran into an unexpected error" });
+      });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
+
+server.get("/api/users", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+server.get("/users", restricted, async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 const port = process.env.PORT || 5000;
