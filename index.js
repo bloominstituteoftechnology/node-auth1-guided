0b8539ab35f133a20f21daa2b0eed18fdc7121b6
Delba <delba.brown@gmail.com>
2019-04-01 09:04:30 +0100

    Add session with cookies

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
+const session = require("express-session"); // 1. Add express session
 
 const db = require("./database/dbConfig.js");
 const Users = require("./users/users-model.js");
 
 const server = express();
 
+// Session is stored in the server
+// Cookie gets sent to the browser, and the cookie has the session id
+
+// 3. Create sessionConfig object
+const sessionConfig = {
+  name: "monkey", // name for session, default is connect.sid but for security purposes we change it
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // length of session in ms
+    secure: false // used for https only, false for dev for now
+  },
+  httpOnly: true, // user cannot access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false // GDPR laws agaisnt setting cookies automatically, need consent from the user
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig)); // 2. Use session and pass a sessionConfig object
 
 server.get("/", (req, res) => {
   res.send("It's alive!");
@@ -27,6 +45,7 @@ server.post("/api/register", (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.user = saved; // Save user so that he won't have to login after registering
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -42,7 +61,10 @@ server.post("/api/login", (req, res) => {
     .then(user => {
       // check that passwords match
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        req.session.user = user; // 4. This
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}! Have a cookie.` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -54,26 +76,35 @@ server.post("/api/login", (req, res) => {
 
 // Protect this route, only authenticated users should see it
 // Middleware
+// 5. Middleware is simplified
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Crendentials" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Ran into an unexpected error" });
-      });
+  if (req.session && res.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: "No credentials provided" });
+    res.status(401).json({ message: "You shall not pass!" });
   }
 }
+
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "Invalid Crendentials" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json({ message: "Ran into an unexpected error" });
+//       });
+//   } else {
+//     res.status(400).json({ message: "No credentials provided" });
+//   }
+// }
 
 server.get("/api/users", restricted, (req, res) => {
   Users.find()
