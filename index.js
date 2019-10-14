5c2c4fb34b6c73edb2924ffb65ca4c273efafdc6
Min-Huang <minh.huang96@gmail.com>
2019-10-14 10:56:28 -0700

    added protect middleware

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+// validate the user
+  
+// hash the password
+  const hash = bcrypt.hashSync(user.password, 12);
+  
+  // we override the password with the hash
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -32,13 +40,16 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
+
+
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+
+      if (user && bcrypt.compare(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
@@ -46,7 +57,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected,(req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -55,14 +66,49 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get("/hash", (req, res) => {
-  // read a password from the Authorization header
+  
   // return an object with the password hashed using bcryptjs
   // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
-  const hash = bcrypt.hashSync(req.body.password, 12)
-  //res.setHeader("password", "text/plain");
-  res.json(hash)
+
+ // read a password from the Authorization header
+  const password = req.headers.authorization;
+
+  if (password) {
+    // that 12 is how we slow down attackers trying to pre-generate hashes
+    const hash = bcrypt.hashSync(password, 12) // the 12 is the number of rounds 2^12
+    // a good starting value is 14
+
+  
+    res.status(200).json({ hash })
+  } else {
+    res.status(400).json({message:"please provide credentials!"})
+  }
 });
 
+// implement the protected middleware that will check for username and password
+// in the headers and if valid provide access to the endpoint
+function protected(req, res, next) {
+  const username = req.headers.user;
+  const password = req.headers.authorization;
+
+  if (!req.body) {
+    res.status(401).json({message:"please provide username and password"})
+  }
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (!user || !bcrypt.compare(password, user.password)) {
+        res.status(401).json({ message: `Invalid Credentials` });
+      } else {
+        next();
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
