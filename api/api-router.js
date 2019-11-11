5da2326e8cde6753f5ff9c06e0d032cf9d5fa083
juarezfrench <markedfrench@gmail.com>
2019-11-11 11:36:43 -0600

    Set up bcrypt

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcrypt').hash();
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -9,5 +9,22 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('./hash'), (req, res) => {
+ // read a password from the body
+  // hash the password using bcryptjs
+  // return it to the user in an object that looks like
+  // { password: 'original passsword', hash: 'hashed password' }
+
+  var salt = bcrypt.genSaltSync(saltRounds);
+  var hash = bcrypt.hashSync(myPlaintextPassword, salt);
+
+};
+
+
+
+
+
+};
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
-
+const configureMiddleware = require('./configure-middleware')
 const server = express();
 
 server.use('/api', apiRouter);
