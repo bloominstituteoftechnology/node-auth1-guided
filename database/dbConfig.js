a62a5173e38155573cff8669a4d8724295f0436b
Luis Hernandez <luishrd@gmail.com>
2019-11-11 09:13:33 -0700

    makes db environment dynamic

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
+const knexfile = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const environment = process.env.NODE_ENV || 'development';
+
+module.exports = knex(knexfile[environment]);
