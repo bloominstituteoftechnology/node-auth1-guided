e2216727ab86172dde99bf902e0d4e0e0bb701c5
Kevin Allen <kevin.allen91@gmail.com>
2019-11-11 09:36:04 -0800

    auth function

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,32 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+var bcrypt = require("bcryptjs");
 
-router.get('/', (req, res) => {
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post("/hash", (req, res) => {
+  // read a password from the body
+  // hash the password using bcryptjs
+  // return it to the user in an objet that looks like
+  // { password: 'original password, hash: 'hashed password'}
+  const password = req.body.password;
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      // Store hash in your password DB.
+      res.status(200).json({
+        password: password,
+        hash: hash
+      });
+    });
+  });
 });
 
 module.exports = router;
