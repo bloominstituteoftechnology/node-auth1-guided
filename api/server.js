641fd7f5a01cbb34acf8b10c518b25ea9ddc5b68
Kevin Allen <kevin.allen91@gmail.com>
2019-11-11 09:21:37 -0800

    configure middleware

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require("./api-router.js");
+const configureMiddleware = require("./configure-middleware");
 
 const server = express();
 
-server.use('/api', apiRouter);
+configureMiddleware(server);
+
+server.use("/api", apiRouter);
 
 module.exports = server;
