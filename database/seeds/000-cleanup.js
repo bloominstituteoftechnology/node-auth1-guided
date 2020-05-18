e83f339af3704d7da2777b850bb7c3b65a223fc9
Luis Hernandez <luishrd@gmail.com>
2020-05-18 10:20:47 -0600

    fixes typo on cleanup

--- database/seeds/000-cleanup.js
+++ database/seeds/000-cleanup.js
@@ -3,7 +3,7 @@ const cleaner = require("knex-cleaner");
 exports.seed = function (knex) {
   return cleaner
     .clean(knex, {
-      model: "delete",
+      mode: "delete",
       restartIdentity: true, // ask PosrgreSQL to reset the Primary Keys back to 0
       ignoreTables: ["knex_migrations", "knex_migrations_lock"],
     })
