c7dc5ae6ed3e21e6634a6db7cc0f5fcc0fb1b6cb
Veto Ramirez <vetorobert3@gmail.com>
2020-06-02 20:28:46 -0700

    getting an error for module bcrypt. Probably a typo.

--- api/server.js
+++ api/server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+
+const sessionConfig = {
+  name: 'vrSession',
+  secret: 'my hotdog has a first name',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+}
 
 const usersRouter = require("../users/users-router.js");
+const authRouter = require('../auth/auth-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
