e6d807ee4d1fcbe2dc7c11bc0cacefb2e668fb51
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-17 17:19:51 +0200

    wip

--- database/seeds/001-init.js
+++ database/seeds/001-init.js
@@ -1,7 +1,6 @@
 exports.seed = function (knex) {
   // 000-cleanup.js already cleaned out all tables
   return knex("users")
-  
     .insert({
       username: 'admin',
       password: '$2a$08$CjOzAqkUXePlNyZCG6TKuubIY.MpjKqOdrV/W3178ah483kyEbeSe', // plain text password is 1234
