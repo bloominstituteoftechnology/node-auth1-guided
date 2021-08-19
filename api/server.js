073ddfcea6b805575363af7040d2482ba42b98bf
shawn2776 <shawn.harrington2776@gmail.com>
2021-08-18 20:26:33 -0700

    finished project

--- api/server.js
+++ api/server.js
@@ -1,23 +1,40 @@
-const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session)
 
 const usersRouter = require('./users/users-router.js');
+const authRouter = require("./auth/auth-router.js");
 
 const server = express();
 
-server.use(express.static(path.join(__dirname, '../client')));
+server.use(session({
+  // config of cookie and session store
+  name: 'monkey', // name of the cookie
+  secret: 'keep it secret, keep it safe!', // env, not code
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // if true, the cookie is not set unless it's an https connection
+    httpOnly: true, // if true, the cookie is not accessible thru document.cookie
+  },
+  resave: false, // some data stores need this set to true
+  saveUninitialized: false, // privacy implications, if false, no cookie is set on the client unless the req.session is changed
+  store: new KnexSessionStore({
+    knex: require('../database/db-config.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // will create table auto
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the db to keep it clean and performant
+  }),
+}));
 server.use(helmet());
 server.use(express.json());
 
 server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
-});
-
-server.use('*', (req, res, next) => {
-  next({ status: 404, message: 'not found!' });
+  res.json({ api: "up"});
 });
 
 server.use((err, req, res, next) => { // eslint-disable-line
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,15 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res, next) => {
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({ message: 'nopz', status: 401})
+  }
+}
+
+router.get("/", restrict, (req, res, next) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
