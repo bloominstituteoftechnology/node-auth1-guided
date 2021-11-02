26d24924a0d06a5003754a944d3ab03a60327004
Ladrillo <gabriel@cabrejas.net>
2021-11-02 14:47:42 +0100

    port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
