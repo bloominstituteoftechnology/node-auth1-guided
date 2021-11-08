b15cb441edea6656bda44df6b169f51665b5cdd1
Ladrillo <gabriel@cabrejas.net>
2021-11-08 16:03:37 +0100

    no returning array

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -16,7 +16,7 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const [id] = await db("users").insert(user, "id");
+  const [id] = await db("users").insert(user);
   return findById(id);
 }
 
