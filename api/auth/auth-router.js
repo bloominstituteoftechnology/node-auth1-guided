41a18d52dde0529602827a3cc5035391b1ab311f
James Blacklock <jamesblacklock@protonmail.com>
2022-08-22 13:33:34 -0400

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -39,6 +39,9 @@ router.post('/login', async (req, res, next) => {
         }
 
         if(bcrypt.compareSync(password, user.password)) {
+
+            req.session.user = user;
+
             res.json({ message: `You are now logged in, ${username}! Here is your id: ${req.session.id}`});
         } else {
             next({ status: 401, message: `Invalid credentials!`});
