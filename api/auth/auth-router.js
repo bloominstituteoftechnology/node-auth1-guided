20a189f979661c7b32634138f4e26be3c5509328
James Blacklock <jamesblacklock@protonmail.com>
2022-08-22 12:47:59 -0400

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,15 +11,19 @@ function usernameIsUnique(req, res, next) {
     next();
 }
 
-router.post('/register', usernameIsUnique, async (req, res) => {
-    const { username, password } = req.body;
+router.post('/register', usernameIsUnique, async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
 
-    const hash = bcrypt.hashSync(password, 12);
+        const hash = bcrypt.hashSync(password, 12);
 
-    const user = { username, password: hash };
-    await Users.add(user);
+        const user = { username, password: hash };
+        await Users.add(user);
 
-    res.status(201).json({ message: `You are now registered, ${username}!`})
+        res.status(201).json({ message: `You are now registered, ${username}!`})
+    } catch(err) {
+        next(err);
+    }
 });
 
 // router.post('/login', (req, res) => {});
